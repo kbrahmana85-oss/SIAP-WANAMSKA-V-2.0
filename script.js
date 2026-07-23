17469f8d117166e1eafa892154153815d3d4debc
kbrahmana85-oss <kbrahmana85@gmail.com>
2026-07-23 11:07:13 +0700

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
    ========================================================================= */
 
 // GANTI dengan URL Web App Apps Script kamu (Deploy > Manage deployments)
-const API_URL = "https://script.google.com/macros/s/AKfycbzMiPXvFMV5q0ykIzRWopEjUa9YyheMpTVd_CPSistXNxhWsIYSlsjG48_QosCUGrIfWQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwiv90b8y2tNpLrpxnA-Azh0FVuIotMm76hSLhOuXaMItZml21huOEQ7SawUxcKf1FdPw/exec";
 
 let sessionToken = "";
 let userRole = "";
