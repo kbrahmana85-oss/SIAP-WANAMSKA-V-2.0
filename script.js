ec0b31a28e3c4d9f5b008ac7979c846c2670985b
kbrahmana85-oss <kbrahmana85@gmail.com>
2026-07-29 13:23:49 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // GANTI dengan URL Web App Apps Script kamu (Deploy > Manage deployments)
-const API_URL = "https://script.google.com/macros/s/AKfycbxLkHTI_-7V2xYuBTTRsxtII3RHS8yTMZ6sUcnb9XULjg_saiGmMdTzMlbB_jSLNXH2Ow/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzeenzGc5iTs41JZfCjxM7dKA_hA5_IInFGV61C4YEq_oGfdkUTBH7Ncq9jdotDcTNRNA/exec";
 
 // VERSI APLIKASI UNTUK RESET CORRUPT PWA CACHE (Ditingkatkan ke 2.1.1)
 const APP_VERSION = "2.1.1"; 
