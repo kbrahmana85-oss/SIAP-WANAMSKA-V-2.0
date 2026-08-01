347cfd6a3a27fe63e30d52e0edfa186179509f5f
kbrahmana85-oss <kbrahmana85@gmail.com>
2026-08-01 09:32:55 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // GANTI dengan URL Web App Apps Script Anda
-const API_URL = "https://script.google.com/macros/s/AKfycbx0TzNLsXZe8zeXUGWfgcmisDPlU-oDUjDZIjxlGoOpggeKwziAJw13aQapfBI2Rs0l0w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwjZ0sONMa_5UEJv4Qh7dvh64XslpjwG9ZA1UGoySZqhUmSi8dF9Lfoq5RsIHtDYqCHFQ/exec";
 
 // VERSI APLIKASI DI-UPDATE KE 2.5.0 UNTUK FIX INVENTARIS, KAS, TANGGAL PROFIL, & KOMPRESI DOKUMENTASI
 const APP_VERSION = "2.5.0"; 
